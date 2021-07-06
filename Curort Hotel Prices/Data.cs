32d98205561d8b61e3ae6de8252f4c992a65286b
unknown <aico60@abv.bg>
2021-07-06 18:48:01 +0300

    Initial

---
+++ Curort Hotel Prices/Data.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resort_Hotel_Prices
+{
+    class Data
+    {
+        public Data() { }
+
+         public List<string> resorts { get; set; } = new List<string>() { "Albena", "Golden Sands", "Kiten", "Pomorie", "Sozopol" };
+         public Dictionary<string, Dictionary<string, double>> hotelsAndPrices { get; set; } = new Dictionary<string, Dictionary<string, double>>()
+        {
+            {
+                "Albena",
+                new Dictionary<string, double>
+                {
+                    {"Flamingo Grand Hotel", 309 },
+                    {"Dobrudzha Hotel", 108 },
+                    {"Gergana", 307},
+                    {"Sandy Beach", 285 },
+                    {"Elitsa", 316 },
+                    {"Compass", 190 }
+                }
+            },
+            {
+                "Sozopol",
+                new Dictionary<string, double>
+                {
+                    {"Sunny", 131 },
+                    {"Radik", 91 },
+                    {"Santa Marina", 224},
+                    {"Lozite", 90 },
+                    {"Martinez", 171 },
+                    {"Selena", 148 }
+                }
+            },
+            {
+                "Golden Sands",
+                new Dictionary<string, double>
+                {
+                    {"Hotel Erma", 98 },
+                    {"Liliya", 127 },
+                    {"Detelina Hotel", 90},
+                    {"Abasador", 92 },
+                    {"Briz", 120 },
+                    {"Admiral", 196 }
+                }
+            },
+            {
+                "Kiten",
+                new Dictionary<string, double>
+                {
+                    {"Kremikovtsi", 75 },
+                    {"Diamond Hotel", 96 },
+                    {"Onyx", 100 },
+                    {"Daniela Wellness", 140 },
+                    {"Elmar", 142 },
+                    {"Milev", 42 }
+                }
+            },
+            {
+                "Pomorie",
+                new Dictionary<string, double>
+                {
+                    {"Paros 2", 55 },
+                    {"Paros", 80 },
+                    {"Paradise", 91},
+                    {"Central Plaza", 132 },
+                    {"Wave Resort", 469 },
+                    {"Helios", 115 }
+                }
+            }
+        };
+    }
+}
---
+++ Curort Hotel Prices/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resort_Hotel_Prices
+{
+    class Program
+    {
+        static void ListResorts()
+        {
+            Data data = new Data();
+            List<string> resorts = data.resorts.ToList();
+            resorts.Sort();
+            for (int i = 0; i < resorts.Count; i++)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, resorts[i]);
+            }
+        }
+        static void ListHotelsAndPrices(string resortName) 
+        {
+            Data data = new Data();
+            Console.Clear();
+            Console.WriteLine("You selected {0}.\n", resortName);
+            foreach (var item in data.hotelsAndPrices.Where(key => key.Key == resortName))
+            {
+                foreach (var hotelNameAndPrice in item.Value)
+                {
+                    Console.WriteLine("Name: {0}, Price: {1}lv", hotelNameAndPrice.Key, hotelNameAndPrice.Value);
+                }
+            }
+            while (true)
+            {
+                Console.Write("\nPress Enter or input 0 to go back: ");
+                int goBack = int.Parse(Console.ReadLine());
+                if (goBack == 0)
+                {
+                    Console.Clear();
+                    break; 
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        { 
+            Data data = new Data();
+            List<string> resorts = data.resorts.ToList();
+            resorts.Sort();
+
+            while (true)
+            {
+                ListResorts();
+                int selection = 0;
+                try
+                {
+                    Console.Write("\nPlease select a resort or enter 0 to exit: ");
+                    selection = int.Parse(Console.ReadLine());
+                    while (selection < 0 || selection > resorts.Count)
+                    {
+                        Console.WriteLine("Please make a valid selection!");
+                        Console.Write("\nPlease select a resort or enter 0 to exit: ");
+                        selection = int.Parse(Console.ReadLine());
+                    }
+                    if (selection == 0)
+                    {
+                        break;
+                    }
+                    ListHotelsAndPrices(resorts[selection - 1]);
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                }
+            }           
+        }
+    }
+}
